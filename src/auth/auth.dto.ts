362dbea030209a0190a8b4447280ab7edf73b6fe
Damien Mauger <mauger.damien@hotmail.fr>
2025-10-26 00:48:55 +0200

    change: validation user

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,9 +1,11 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class LoginDto {
   @IsNotEmpty()
+  @IsEmail()
   email: string;
 
   @IsNotEmpty()
+  @MinLength(8)
   password: string;
 }
