73dd18e323e6f7c80a39b42fa06f39a66a35b5a9
Damien Mauger <mauger.damien@hotmail.fr>
2025-11-26 19:59:17 +0100

    fix: dossier images non exposé

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
@@ -6,9 +7,16 @@ import { ActivitesModule } from './activites/activites.module';
 import { ThemesModule } from './themes/themes.module';
 import { PointsFortModule } from './points-fort/points-fort.module';
 import { ImagesModule } from './images/images.module';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveStaticOptions: {
+        fallthrough: false,
+      },
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
